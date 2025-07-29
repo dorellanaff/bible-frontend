ee98d43f430c1d998c2ae1710cf6cd46839e5140
Daniel Orellana <daniore2211@gmail.com>
2025-07-29 16:01:52 +0000

    el componente de libros, no se ajusta hasta abajo de toda la pantalla

--- src/components/bible/book-selector.tsx
+++ src/components/bible/book-selector.tsx
@@ -34,23 +34,23 @@ function BookList({ books, selectedBook, onBookSelect }: { books: Book[], select
 
 export function BookSelector({ oldTestamentBooks, newTestamentBooks, selectedBook, onBookSelect }: BookSelectorProps) {
   return (
-    <Card className="card-material">
+    <Card className="card-material lg:flex lg:flex-col lg:h-[calc(100vh-5rem)]">
       <CardHeader>
         <CardTitle className="font-headline text-2xl">Seleccionar Libro</CardTitle>
       </CardHeader>
-      <CardContent>
-        <Tabs defaultValue="antiguo" className="w-full">
+      <CardContent className="flex-grow flex flex-col">
+        <Tabs defaultValue="antiguo" className="w-full flex-grow flex flex-col">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="antiguo" className="font-headline">Antiguo Testamento</TabsTrigger>
             <TabsTrigger value="nuevo" className="font-headline">Nuevo Testamento</TabsTrigger>
           </TabsList>
-          <TabsContent value="antiguo" className="mt-4">
-              <ScrollArea className="h-96">
+          <TabsContent value="antiguo" className="mt-4 flex-grow">
+              <ScrollArea className="h-full">
                 <BookList books={oldTestamentBooks} selectedBook={selectedBook} onBookSelect={onBookSelect} />
               </ScrollArea>
           </TabsContent>
-          <TabsContent value="nuevo" className="mt-4">
-            <ScrollArea className="h-96">
+          <TabsContent value="nuevo" className="mt-4 flex-grow">
+            <ScrollArea className="h-full">
               <BookList books={newTestamentBooks} selectedBook={selectedBook} onBookSelect={onBookSelect} />
             </ScrollArea>
           </TabsContent>
