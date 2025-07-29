bc163aa2d1f921ddfe93612e93c4a2d1bd4bbc63
Daniel Orellana <daniore2211@gmail.com>
2025-07-29 18:46:18 +0000

    no esta centrado el loading la animacion

--- src/components/bible/loading-animation.tsx
+++ src/components/bible/loading-animation.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export function LoadingAnimation() {
   return (
-    <div className="fixed inset-0 bg-background flex flex-col items-center justify-center z-50">
+    <div className="book-loading-container">
       <div className="book">
         <div className="book__pg-shadow"></div>
         <div className="book__pg"></div>
@@ -13,7 +13,7 @@ export function LoadingAnimation() {
         <div className="book__pg book__pg--4"></div>
         <div className="book__pg book__pg--5"></div>
       </div>
-      <h2 className="text-2xl font-headline text-primary mt-8">Cargando...</h2>
+      <h2 className="text-2xl font-headline text-primary mt-28">Cargando...</h2>
     </div>
   );
 }
