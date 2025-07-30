918fcadb5fe9633e4163d294a871ad16f7141126
Daniel Orellana <daniore2211@gmail.com>
2025-07-30 03:03:56 +0000

    quita el logo del loading y coloca una progreso circular

--- src/components/bible/loading-animation.tsx
+++ src/components/bible/loading-animation.tsx
@@ -20,16 +20,9 @@ export function LoadingAnimation() {
   }, []);
 
   return (
-    <div className="book-loading-container">
-      <div className="book">
-        <div className="book__pg-shadow"></div>
-        <div className="book__pg"></div>
-        <div className="book__pg book__pg--2"></div>
-        <div className="book__pg book__pg--3"></div>
-        <div className="book__pg book__pg--4"></div>
-        <div className="book__pg book__pg--5"></div>
-      </div>
-      <h2 className="text-2xl font-headline text-primary mt-28">
+    <div className="loading-container">
+      <div className="circular-progress"></div>
+      <h2 className="text-2xl font-headline text-primary mt-8">
         Una biblia para ti{dots}
       </h2>
     </div>
