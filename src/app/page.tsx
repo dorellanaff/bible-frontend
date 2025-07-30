2c4cb104e472032139638614428bbd7d529b3e2a
Daniel Orellana <daniore2211@gmail.com>
2025-07-30 04:01:33 +0000

    la card de libros y la card de lectura del libro no inician en la misma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -218,7 +218,6 @@ export default function Home() {
     if (selectedBook) {
       setChapterSelectorOpen(true);
     }
-    chapterViewerRef.current?.scrollIntoView({ behavior: 'smooth' });
   }
   
   const handleChapterSelect = (selectedChapter: number) => {
@@ -346,8 +345,8 @@ export default function Home() {
         comparisonVersions={comparisonVersions}
         onToggleComparisonVersion={handleToggleComparisonVersion}
       />
-      <main className="flex-1 flex overflow-hidden p-4 sm:p-6 lg:p-8">
-        <div className="flex flex-col lg:flex-row gap-8 w-full h-full">
+      <main className="flex-1 flex overflow-hidden">
+        <div className="flex flex-col lg:flex-row gap-8 w-full h-full p-4 sm:p-6 lg:p-8 lg:items-start">
           <aside className={cn(
             "w-full lg:w-1/3 xl:w-1/4 h-full",
             { 'hidden lg:block': showMobileReadingView, 'block': !showMobileReadingView }
